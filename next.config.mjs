e9f8a025d72cdd528a98d0eff359adb95996c4a4
sdbp-admin <sdgp.admin@gmail.com>
2026-08-11 01:45:51 +0200

    chore: prepare prototype for GitHub Pages

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/governance",
+  assetPrefix: "/governance/",
+  trailingSlash: true,
+};
+
+export default nextConfig;
